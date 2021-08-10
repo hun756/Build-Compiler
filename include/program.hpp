cd44a4410c9ed15702024c3d625ceaaf500e4631
hun756 <ekemenms@hotmail.com>
2021-08-10 22:01:02 +0300

    Fix App Template

---
+++ include/program.hpp
@@ -0,0 +1,19 @@
+#ifndef PROGRAM_HPP
+#define PROGRAM_HPP
+
+#include <vector>
+#include <string>
+
+namespace Compiler
+{
+    class Program
+    {
+        public:
+        static void Main(std::vector<std::string> args)
+        {
+            
+        }
+    };
+} // namespace Compiler
+
+#endif /* end of include guard :  PROGRAM_HPP */
