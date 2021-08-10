4ee8036ec5304210fd2c33a0f4f03ff36eacbcd9
hun756 <ekemenms@hotmail.com>
2021-08-10 22:38:46 +0300

    Add CSharp Styple Properties

---
+++ include/helper/var.hpp
@@ -0,0 +1,39 @@
+#ifndef VARIABLE_PROPERTY_HPP
+#define VARIABLE_PROPERTY_HPP
+
+#include <functional>
+
+template <typename T>
+class Var
+{
+public:
+    Var(
+        std::function<void(T)> setter,
+        std::function<T()> getter
+    )   :   _setter(setter), _getter(getter)
+    {
+    }
+
+    operator T() const 
+    { 
+        return _getter(); 
+    }
+
+    Var<T>& operator=(const T &value)
+    {
+        _setter(value);
+        return *this;
+    }
+
+    T& value() 
+    { 
+        return _value; 
+    }
+
+private:
+    std::function<void(T)> _setter;
+    std::function<T()> _getter;
+    T _value;
+};
+
+#endif /* end of include guard :  VARIABLE_PROPERTY_HPP */
